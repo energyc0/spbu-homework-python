2592c408677368e1aaf7341c11ccba2cbd17a3d1
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-09-13 14:32:30 +0300

    extended gcd algorithm

---
+++ src/extended_euclid.py
@@ -0,0 +1,10 @@
+def extended_gcd(a,b):
+    if b == 0:
+        return a, 1, 0
+    else:
+        d, x, y = extended_gcd(b, a % b)
+        return d, y, x - y * (a // b)
+
+a = int(input("a: "))
+b = int(input("b: "))
+print(extended_gcd(max(a, b), min(a, b)))
